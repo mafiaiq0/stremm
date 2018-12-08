22ee8e2a34d3132aeaa7d666a3da955d8745ac26
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-12-08 23:27:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,27 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "V";
-var adminprefix = 'V'
+var prefix = ".";
+var adminprefix = '.'
 
-const developers = ["353708472690540564","id"]
+const developers = ["521001769082552335","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
+      message.channel.send(`**✅**`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
+      message.channel.send(`**✅**`)
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
+      message.channel.send(`**✅**`)
   } else 
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
@@ -29,11 +29,11 @@ client.on('message', message => {
   }
   if (message.content.startsWith(adminprefix + 'na')) {
   client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
+      message.channel.send(`**✅** `)
 } else
 if (message.content.startsWith(adminprefix + 'av')) {
   client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+    message.channel.send(`**✅** `);
 }
 });
 
