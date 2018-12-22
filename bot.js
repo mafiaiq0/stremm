c289594556d81a0588c23c8a4827d64ceea732c6
mafiaiq0 <46052057+mafiaiq0@users.noreply.github.com>
2018-12-22 19:59:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = ".";
 var adminprefix = '.'
 
-const developers = ["353708472690540564","id"]
+const developers = ["422403573767340043","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
